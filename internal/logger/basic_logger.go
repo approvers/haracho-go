c258156aa268f0763956ff48121d11422360f293
mirror-kt <me@mirror-kt.dev>
2020-04-08 20:18:56 +0900

    add ~ln for loggers

--- internal/logger/basic_logger.go
+++ internal/logger/basic_logger.go
@@ -19,3 +19,15 @@ func (bl *BasicLogger) Errorf(format string, v ...interface{}) {
 func (bl *BasicLogger) Debugf(format string, v ...interface{}) {
 	bl.Logger.Printf("[Debug] "+format, v)
 }
+
+func (bl *BasicLogger) Infofln(format string, v ...interface{}) {
+	bl.Logger.Printf("[Info] "+format+"\n", v)
+}
+
+func (bl *BasicLogger) Errorfln(format string, v ...interface{}) {
+	bl.Logger.Printf("[Error] "+format+"\n", v)
+}
+
+func (bl *BasicLogger) Debugfln(format string, v ...interface{}) {
+	bl.Logger.Printf("[Debug] "+format+"\n", v)
+}
--- internal/logger/logger.go
+++ internal/logger/logger.go
@@ -4,4 +4,8 @@ type Logger interface {
 	Infof(string, ...interface{})
 	Errorf(string, ...interface{})
 	Debugf(string, ...interface{})
+
+	Infofln(string, ...interface{})
+	Errorfln(string, ...interface{})
+	Debugfln(string, ...interface{})
 }
